8581e15423d4b056f01662191d61d116bef76850
nextgensparx <siphomateke@gmail.com>
2018-02-05 12:20:20 +0200

    Use custom promise request, standardise output
    xhrRequestXml now returns headers as well as an object.

--- src/node/ajax.js
+++ src/node/ajax.js
@@ -2,7 +2,7 @@
 import {
   XhrError,
 } from '@/error';
-import request from 'request-promise-native';
+import request from 'request';
 import xml2js from 'xml2js';
 
 /**
@@ -16,34 +16,54 @@ import xml2js from 'xml2js';
  */
 
 /**
+ * @typedef xhrRequestResponse
+ * @property {request.RequestResponse} response
+ * @property {any} body
+ */
+
+/**
  *
  * @param {xhrRequestOptions} options
- * @return {request.RequestPromise}
+ * @return {Promise<xhrRequestResponse>}
  */
 function xhrRequest(options) {
-  return request({
-    url: options.url,
-    method: options.method,
-    headers: options.requestHeaders
+  return new Promise((resolve, reject) => {
+    return request({
+      url: options.url,
+      method: options.method,
+      headers: options.requestHeaders
+    }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response) {
+        if (response.statusCode >= 200 && response.statusCode < 400) {
+          resolve({response, body});
+        } else {
+          reject(new XhrError('xhr_invalid_status', 'XHR status invalid; '+response.statusMessage));
+        }
+      } else {
+        reject(new XhrError('xhr_error','Unknown XHR error; '));
+      }
+    });
   });
 }
 
 /**
  *
  * @param {xhrRequestOptions} xhrOptions
- * @return {Promise<XMLHttpRequest>}
+ * @return {Promise<any>}
  */
 function xhrRequestXml(xhrOptions) {
   xhrOptions = Object.assign({
     mimeType: 'application/xml',
   }, xhrOptions);
   return new Promise((resolve, reject) => {
-    xhrRequest(xhrOptions).then((str) => {
-      xml2js.parseString(str, (err, data) => {
+    xhrRequest(xhrOptions).then(({response, body}) => {
+      xml2js.parseString(body, (err, data) => {
         if (err) {
           reject(new XhrError('xhr_invalid_xml', err));
         } else {
-          resolve(data);
+          resolve({data, headers: response.headers});
         }
       });
     });
