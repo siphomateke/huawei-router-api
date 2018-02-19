6eff79caeb76f48da5f69d76a6d2834a32fc32ae
nextgensparx <siphomateke@gmail.com>
2018-02-19 11:31:20 +0200

    Restructure getting configs to be DRY

--- src/config.js
+++ src/config.js
@@ -2,6 +2,163 @@
 import * as ajax from '@/ajax';
 import * as utils from '@/utils';
 
+class ApiConfig {
+  /**
+   * @typedef ApiConfigOptions
+   * @property {function} [map]
+   * @property {function} [converter]
+   */
+
+  /**
+   * @param {string} url
+   * @param {ApiConfigOptions} options
+   */
+  constructor(url, options={}) {
+    this.url = url;
+    this.options = options;
+  }
+
+  async get() {
+    const data = await ajax.getAjaxData({url: this.url});
+    let processed = data;
+    if (this.options.map) {
+      processed = {};
+      for (const key in data) {
+        if (Object.prototype.hasOwnProperty.call(data, key)) {
+          processed[key] = this.options.map(data[key]);
+        }
+      }
+    }
+    if (this.options.converter) {
+      processed = this.options.converter(processed);
+    }
+    return processed;
+  }
+}
+
+// TODO: Add config checks and throw errors
+
+/*
+  'autoapn_enabled': g_feature.autoapn_enabled === '1',
+  'checklogin_enabled': g_feature.login === '1',
+  'ap_station_enabled': g_feature.ap_station_enabled === '1',
+  'voip_adcance_enable': voiceadvancesetting === '1',
+  */
+
+/**
+ * @typedef ConfigModuleSwitch
+ * @property {boolean} ussd_enabled
+ * @property {boolean} bbou_enabled
+ * @property {boolean} sms_enabled
+ * @property {boolean} sdcard_enabled
+ * @property {boolean} wifi_enabled
+ * @property {boolean} statistic_enabled
+ * @property {boolean} help_enabled
+ * @property {boolean} stk_enabled
+ * @property {boolean} pb_enabled
+ * @property {boolean} dlna_enabled
+ * @property {boolean} ota_enabled
+ * @property {boolean} wifioffload_enabled
+ * @property {boolean} cradle_enabled
+ * @property {boolean} multssid_enable
+ * @property {boolean} ipv6_enabled
+ * @property {boolean} monthly_volume_enabled
+ * @property {boolean} powersave_enabled
+ * @property {boolean} sntp_enabled
+ * @property {boolean} encrypt_enabled
+ * @property {boolean} dataswitch_enabled
+ * @property {boolean} ddns_enabled
+ * @property {boolean} sambashare_enabled
+ * @property {boolean} poweroff_enabled
+ * @property {boolean} fw_macfilter_enabled
+ * @property {boolean} ecomode_enabled
+ * @property {boolean} zonetime_enabled
+ * @property {boolean} diagnosis_enabled
+ * @property {boolean} localupdate_enabled
+ * @property {boolean} cbs_enabled
+ * @property {boolean} voip_enabled
+ * @property {boolean} qrcode_enabled
+ * @property {boolean} charger_enbaled
+ * @property {boolean} vpn_enabled
+ * @property {boolean} cs_enable
+ * @property {boolean} tr069_enabled
+ * @property {boolean} antenna_enabled
+ * @property {boolean} aclui_enabled
+ * @property {boolean} static_route_enabled
+ * @property {boolean} static_route6_enabled
+ * @property {boolean} loginusername_enable
+ */
+
+/**
+ * @typedef PublicKey
+ * @property {string} n
+ * @property {string} e
+ */
+
+/**
+ * @typedef EncryptionConfig
+ * @property {PublicKey} publicKey Public RSA keys
+ */
+
+/**
+ * @typedef SmsConfig
+ * @property {number} cbsenable
+ * @property {number} cdma_enabled
+ * @property {number} enable
+ * @property {number} getcontactenable
+ * @property {number} import_enabled
+ * @property {number} localmax
+ * @property {number} maxphone
+ * @property {number} pagesize
+ * @property {number} session_sms_enabled
+ * @property {number} sms_center_enabled
+ * @property {number} sms_priority_enabled
+ * @property {number} sms_validity_enabled
+ * @property {number} smscharlang
+ * @property {number} smscharmap
+ * @property {number} smsfulltype
+ * @property {number} url_enabled
+ * @property {number} validity
+ */
+
+/**
+ * @typedef UssdConfigMenuItem
+ * @property {string} Name
+ * @property {string} Command
+ */
+
+/**
+ * @typedef UssdConfigMenu
+ * @property {UssdConfigMenuItem[]} MenuItem
+ */
+
+/**
+ * @typedef UssdConfigGeneral
+ * @property {string} Action
+ * @property {string} Description
+ * @property {string} LimitText
+ * @property {UssdConfigMenu} Menu
+ * @property {string} Title
+ */
+
+/**
+ * @typedef UssdConfig
+ * @property {UssdConfigGeneral} General
+ */
+
+/** @type {Object.<string, ApiConfig>}*/
+let apiConfigs = {
+  module: new ApiConfig('api/global/module-switch', {map: item => item === 1}),
+  sms: new ApiConfig('config/sms/config.xml', {map: item => parseInt(item)}),
+  prepaidussd: new ApiConfig('config/ussd/prepaidussd.xml'),
+  postpaidussd: new ApiConfig('config/ussd/postpaidussd.xml'),
+  enc: new ApiConfig('api/webserver/publickey', {
+    converter: data => {
+      return {publicKey: {n: data.encpubkeyn, e: data.encpubkeye}};
+    },
+  }),
+};
+
 export default {
   username: null,
   password: null,
@@ -10,18 +167,18 @@ export default {
   ussdWaitInterval: 1000,
   ussdTimeout: 20000,
   api: {
+    /** @type {ConfigModuleSwitch} */
     module: null,
-    /**
-     * @property {object} publicKey Public RSA keys
-     * @property {string} publicKey.n
-     * @property {string} publicKey.e
-     */
+    /** @type {EncryptionConfig} */
     encryption: {
       publicKey: null,
     },
+    /** @type {SmsConfig} */
     sms: null,
     ussd: {
+      /** @type {UssdConfig} */
       prepaid: null,
+      /** @type {UssdConfig} */
       postpaid: null,
     },
   },
@@ -62,157 +219,39 @@ export default {
     return this.parsedUrl;
   },
 
-  // TODO: Add config checks and throw errors
-
-  /*
-  'autoapn_enabled': g_feature.autoapn_enabled === '1',
-  'checklogin_enabled': g_feature.login === '1',
-  'ap_station_enabled': g_feature.ap_station_enabled === '1',
-  'voip_adcance_enable': voiceadvancesetting === '1',
-  */
-
   /**
-   * @typedef ConfigModuleSwitch
-   * @property {boolean} ussd_enabled
-   * @property {boolean} bbou_enabled
-   * @property {boolean} sms_enabled
-   * @property {boolean} sdcard_enabled
-   * @property {boolean} wifi_enabled
-   * @property {boolean} statistic_enabled
-   * @property {boolean} help_enabled
-   * @property {boolean} stk_enabled
-   * @property {boolean} pb_enabled
-   * @property {boolean} dlna_enabled
-   * @property {boolean} ota_enabled
-   * @property {boolean} wifioffload_enabled
-   * @property {boolean} cradle_enabled
-   * @property {boolean} multssid_enable
-   * @property {boolean} ipv6_enabled
-   * @property {boolean} monthly_volume_enabled
-   * @property {boolean} powersave_enabled
-   * @property {boolean} sntp_enabled
-   * @property {boolean} encrypt_enabled
-   * @property {boolean} dataswitch_enabled
-   * @property {boolean} ddns_enabled
-   * @property {boolean} sambashare_enabled
-   * @property {boolean} poweroff_enabled
-   * @property {boolean} fw_macfilter_enabled
-   * @property {boolean} ecomode_enabled
-   * @property {boolean} zonetime_enabled
-   * @property {boolean} diagnosis_enabled
-   * @property {boolean} localupdate_enabled
-   * @property {boolean} cbs_enabled
-   * @property {boolean} voip_enabled
-   * @property {boolean} qrcode_enabled
-   * @property {boolean} charger_enbaled
-   * @property {boolean} vpn_enabled
-   * @property {boolean} cs_enable
-   * @property {boolean} tr069_enabled
-   * @property {boolean} antenna_enabled
-   * @property {boolean} aclui_enabled
-   * @property {boolean} static_route_enabled
-   * @property {boolean} static_route6_enabled
-   * @property {boolean} loginusername_enable
+   * @param {string} name The name of the config to retrieve
+   * @param {boolean} fresh Set to true to refresh cached values
+   * @return {Promise<any>}
    */
+  async getConfig(name, fresh=false) {
+    if (!this.api[name] || fresh) {
+      this.api[name] = await apiConfigs[name].get();
+    }
+    return this.api[name];
+  },
 
   /**
    * @return {Promise<ConfigModuleSwitch>}
    */
   async getModuleSwitch() {
-    if (!this.api.module) {
-      const data = await ajax.getAjaxData({url: 'api/global/module-switch'});
-      this.api.module = {};
-      for (const key in data) {
-        if (Object.prototype.hasOwnProperty.call(data, key)) {
-          this.api.module[key] = data[key] === '1';
-        }
-      }
-    }
-    return this.api.module;
+    return this.getConfig('module');
   },
 
   /**
-   * @typedef ApiWebserverPublicKey
-   * @property {string} encpubkeyn
-   * @property {string} encpubkeye
+   * @return {Promise<PublicKey>}
    */
-
   async getPublicEncryptionKey() {
-    if (!this.api.encryption.publicKey) {
-      /**
-       * @type {ApiWebserverPublicKey}
-       */
-      const data = await ajax.getAjaxData({url: 'api/webserver/publickey'});
-      this.api.encryption.publicKey = {};
-      this.api.encryption.publicKey.n = data.encpubkeyn;
-      this.api.encryption.publicKey.e = data.encpubkeye;
-      return this.api.encryption.publicKey;
-    }
-    return this.api.encryption.publicKey;
+    return (await this.getConfig('enc')).publicKey;
   },
-
-  /**
-   * @typedef SmsConfig
-   * @property {number} cbsenable
-   * @property {number} cdma_enabled
-   * @property {number} enable
-   * @property {number} getcontactenable
-   * @property {number} import_enabled
-   * @property {number} localmax
-   * @property {number} maxphone
-   * @property {number} pagesize
-   * @property {number} session_sms_enabled
-   * @property {number} sms_center_enabled
-   * @property {number} sms_priority_enabled
-   * @property {number} sms_validity_enabled
-   * @property {number} smscharlang
-   * @property {number} smscharmap
-   * @property {number} smsfulltype
-   * @property {number} url_enabled
-   * @property {number} validity
-   */
 
   /**
    * Get's SMS configuration
    * @return {Promise<SmsConfig>}
    */
   async getSmsConfig() {
-    if (!this.api.sms) {
-      const data = await ajax.getAjaxData({url: 'config/sms/config.xml'});
-      this.api.sms = {};
-      for (const key in data) {
-        if (Object.prototype.hasOwnProperty.call(data, key)) {
-          this.api.sms[key] = parseInt(data[key]);
-        }
-      }
-    }
-    return this.api.sms;
+    return this.getConfig('sms');
   },
-
-  /**
-   * @typedef UssdConfigMenuItem
-   * @property {string} Name
-   * @property {string} Command
-   */
-
-  /**
-   * @typedef UssdConfigMenu
-   * @property {UssdConfigMenuItem[]} MenuItem
-   */
-
-  /**
-   * @typedef UssdConfigGeneral
-   * @property {string} Action
-   * @property {string} Description
-   * @property {string} LimitText
-   * @property {UssdConfigMenu} Menu
-   * @property {string} Title
-   */
-
-  /**
-   * @typedef UssdConfig
-   * @property {UssdConfigGeneral} General
-   */
 
   /**
    * Get's USSD configuration. Includes USSD commands.
@@ -220,11 +259,7 @@ export default {
    * @return {Promise<UssdConfig>}
    */
   async getUssdConfig(postpaid=false) {
-    let paidType = postpaid ? 'postpaid' : 'prepaid';
-    if (!this.api.ussd[paidType]) {
-      const data = await ajax.getAjaxData({url: `config/ussd/${paidType}ussd.xml`});
-      this.api.ussd[paidType] = data.USSD;
-    }
-    return this.api.ussd[paidType];
+    const paidType = postpaid ? 'postpaid' : 'prepaid';
+    return (await this.getConfig(paidType + 'ussd')).USSD;
   },
 };
